d65b24e7ada080aab9713e7f4decb92eb06dbd85
ax003d <ax003d@gmail.com>
2012-11-18 11:23:01 +0800

    * do post request, but 403 responsed

--- src/com/indax/taskmanager/LoginActivity.java
+++ src/com/indax/taskmanager/LoginActivity.java
@@ -1,6 +1,13 @@
 package com.indax.taskmanager;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -64,13 +71,56 @@ public class LoginActivity extends Activity implements OnClickListener {
 			String username = (String) params[0];
 			String password = (String) params[1];
 			Log.d(TAG, username + password);
+			
+			InputStream is = null;
+			int len = 500;
+			try {
+				URL url = new URL("http://192.168.2.92:8000/accounts/login/");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setReadTimeout(10000 /* milliseconds */);
+		        conn.setConnectTimeout(15000 /* milliseconds */);
+		        conn.setRequestMethod("POST");
+		        conn.setDoInput(true);
+		        conn.connect();
+		        int response = conn.getResponseCode();
+		        Log.d(TAG, "The response is: " + response);
+		        is = conn.getInputStream();
+		        String contentAsString = readIt(is, len);
+		        Log.d(TAG, contentAsString);
+		        return contentAsString;
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if ( is != null ) {
+					try {
+						is.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 			return "";
+						
 		}
 		
 		@Override
 		protected void onPostExecute(Object result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
+		}
+		
+		// Reads an InputStream and converts it to a String.
+		public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+		    Reader reader = null;
+		    reader = new InputStreamReader(stream, "UTF-8");        
+		    char[] buffer = new char[len];
+		    reader.read(buffer);
+		    return new String(buffer);
 		}
 	}
 }
