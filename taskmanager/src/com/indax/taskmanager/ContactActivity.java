e034af3d2b1518640b4411a1376c34ae9b43f842
ax003d <ax003d@gmail.com>
2012-12-22 00:24:20 +0800

    * move event codes to event fragment

--- taskmanager/src/com/indax/taskmanager/ContactActivity.java
+++ taskmanager/src/com/indax/taskmanager/ContactActivity.java
@@ -1,47 +1,27 @@
 package com.indax.taskmanager;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.widget.LinearLayout;
 
-import com.actionbarsherlock.ActionBarSherlock.OnMenuItemSelectedListener;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.ActionBar.TabListener;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.Window;
-import com.indax.taskmanager.adapter.EventListAdapter;
-import com.indax.taskmanager.api.ITaskManagerAPI;
-import com.indax.taskmanager.api.TaskManagerAPI;
 import com.indax.taskmanager.fragments.ContactFragment;
 import com.indax.taskmanager.fragments.EventFragment;
-import com.indax.taskmanager.models.Event;
 
 public class ContactActivity extends SherlockFragmentActivity implements
-		LoaderManager.LoaderCallbacks<Cursor>, OnMenuItemSelectedListener,
-		TabListener {
+		LoaderManager.LoaderCallbacks<Cursor>, TabListener {
 
 	private static final int CONTACT_LOADER = 0;
 	private Uri mContactURL;
-	private EventListAdapter event_adapter;
-	private ITaskManagerAPI api_client;
 	private String mContact;
 	private final String[] tabs = { "Contact", "Events" };
 	private ContactFragment mFragContact;
@@ -61,17 +41,9 @@ public class ContactActivity extends SherlockFragmentActivity implements
 			getSupportActionBar().addTab(tab);
 		}
 
-		api_client = TaskManagerAPI.getInstance(getApplicationContext());
-		event_adapter = new EventListAdapter();
 		mContactURL = getIntent().getData();
-
 		getSupportLoaderManager().initLoader(CONTACT_LOADER, null, this);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getSupportMenuInflater().inflate(R.menu.activity_contact, menu);
-		return true;
+		setSupportProgressBarIndeterminateVisibility(false);
 	}
 
 	@Override
@@ -93,85 +65,11 @@ public class ContactActivity extends SherlockFragmentActivity implements
 			if (mFragContact != null) {
 				mFragContact.setContact(mContact);
 			}
-			new GetEvent().execute(mContact);
 		}
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
-	}
-
-	private class GetEvent extends AsyncTask<String, LinearLayout, JSONObject> {
-
-		@Override
-		protected JSONObject doInBackground(String... params) {
-			JSONObject ret = null;
-			try {
-				if (params.length == 1) {
-					ret = api_client.event(null, params[0], null);
-				} else if (params.length == 2) {
-					ret = api_client.event(params[0], params[1], null);
-				}
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			return ret;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			setSupportProgressBarIndeterminateVisibility(true);
-		}
-
-		@Override
-		protected void onPostExecute(JSONObject result) {
-			super.onPostExecute(result);
-
-			if (result != null && result.has("objects")) {
-				JSONArray jEvents;
-				try {
-					jEvents = result.getJSONArray("objects");
-					for (int i = 0; i < jEvents.length(); i++) {
-						Event event = new Event(jEvents.getJSONObject(i));
-						event_adapter.addEvent(event);
-					}
-					event_adapter.notifyDataSetChanged();
-					setSupportProgressBarIndeterminateVisibility(false);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			}
-
-			// if (result != null && result.has("meta")) {
-			// String next;
-			// try {
-			// next = result.getJSONObject("meta").getString("next");
-			// if (!next.equals("null")) {
-			// new GetEvent().execute(next, mContact);
-			// }
-			// } catch (JSONException e) {
-			// e.printStackTrace();
-			// }
-			// }
-		} // onPostExecutemContact
-	} // GetEvent
-
-	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_log:
-			Intent intent = new Intent(getApplicationContext(),
-					EventActivity.class);
-			intent.putExtra("contact", mContact);
-			startActivity(intent);
-			break;
-		}
-		return super.onMenuItemSelected(featureId, item);
 	}
 
 	@Override
@@ -184,7 +82,7 @@ public class ContactActivity extends SherlockFragmentActivity implements
 		} else if (tab.getText().equals("Events")) {
 			if (mFragEvent == null) {
 				mFragEvent = new EventFragment();
-				mFragEvent.setEventAdapter(event_adapter);
+				mFragEvent.setContact(mContact);
 			}
 			ft.replace(R.id.frag_container, mFragEvent);
 		}
--- taskmanager/src/com/indax/taskmanager/fragments/EventFragment.java
+++ taskmanager/src/com/indax/taskmanager/fragments/EventFragment.java
@@ -1,33 +1,140 @@
 package com.indax.taskmanager.fragments;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.indax.taskmanager.EventActivity;
 import com.indax.taskmanager.R;
 import com.indax.taskmanager.adapter.EventListAdapter;
+import com.indax.taskmanager.api.ITaskManagerAPI;
+import com.indax.taskmanager.api.TaskManagerAPI;
+import com.indax.taskmanager.models.Event;
 
 public class EventFragment extends SherlockFragment {
 
 	private EventListAdapter event_adapter;
+	private String mContact;
+	private ITaskManagerAPI api_client;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		setHasOptionsMenu(true);
 		return inflater.inflate(R.layout.fragment_events, container, false);
 	}
 	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		api_client = TaskManagerAPI.getInstance(getActivity()
+				.getApplicationContext());
 		ListView lst_events = (ListView) getActivity().findViewById(R.id.lst_events);
+		event_adapter = new EventListAdapter();
 		lst_events.setAdapter(event_adapter);
+		new GetEvent().execute(mContact);
 	}
 	
 	public void setEventAdapter(EventListAdapter adapter) {
 		event_adapter = adapter;
 	}
+
+	public void setContact(String name) {
+		mContact = name;
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.fragment_event, menu);
+	}	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_log:
+			Intent intent = new Intent(getActivity().getApplicationContext(),
+					EventActivity.class);
+			intent.putExtra("contact", mContact);
+			startActivity(intent);
+			break;
+		}		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private class GetEvent extends AsyncTask<String, LinearLayout, JSONObject> {
+
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			JSONObject ret = null;
+			try {
+				if (params.length == 1) {
+					ret = api_client.event(null, params[0], null);
+				} else if (params.length == 2) {
+					ret = api_client.event(params[0], params[1], null);
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return ret;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			getSherlockActivity().setSupportProgressBarIndeterminateVisibility(true);
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			super.onPostExecute(result);
+
+			if (result != null && result.has("objects")) {
+				JSONArray jEvents;
+				try {
+					jEvents = result.getJSONArray("objects");
+					for (int i = 0; i < jEvents.length(); i++) {
+						Event event = new Event(jEvents.getJSONObject(i));
+						event_adapter.addEvent(event);
+					}
+					event_adapter.notifyDataSetChanged();
+					getSherlockActivity().setSupportProgressBarIndeterminateVisibility(false);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// if (result != null && result.has("meta")) {
+			// String next;
+			// try {
+			// next = result.getJSONObject("meta").getString("next");
+			// if (!next.equals("null")) {
+			// new GetEvent().execute(next, mContact);
+			// }
+			// } catch (JSONException e) {
+			// e.printStackTrace();
+			// }
+			// }
+		} // onPostExecutemContact
+	} // GetEvent	
 }
